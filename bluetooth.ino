052182c6a8bc9bdab1bb4ef659a6922ee05207b2
Joe s kolengaden <35306176+joeskolengaden@users.noreply.github.com>
2018-03-11 19:35:42 +0530

    bluetooth simple

---
+++ bluetooth.ino
@@ -0,0 +1,14 @@
+char data = 0;  
+void setup() {
+
+Serial.begin(9600); 
+}
+
+void loop() {
+ if(Serial.available() > 0)      // Send data only when you receive data:
+   {
+    data = Serial.read();
+    Serial.print(data); 
+    Serial.print("\n");
+   }
+}
