d43a08be615bab4dfe57cb3a580be8efef890505
Joe s kolengaden <35306176+joeskolengaden@users.noreply.github.com>
2018-10-24 22:39:30 +0530

    Update bluetooth.ino

--- bluetooth.ino
+++ bluetooth.ino
@@ -7,7 +7,7 @@ Serial.begin(9600);
 void loop() {
  if(Serial.available() > 0)      // Send data only when you receive data:
    {
-    data = Serial.read();
+    data = Serial.read();       //read data from bluetooth
     Serial.print(data); 
     Serial.print("\n");
    }
